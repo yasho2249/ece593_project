f4c6eb723cb48064b29838be3a7a2a0e41047c1c
Supreet Gulavani <sgsupreetgulavani@gmail.com>
2022-05-27 18:25:17 -0700

    new addd

--- pipelined_picoblaze_hdl/tb/tb_generator.sv
+++ pipelined_picoblaze_hdl/tb/tb_generator.sv
@@ -10,7 +10,28 @@ Since the picoblaze architecture reads only from the ROM, we have created a gene
 in the form of .mem files. 
 
 */
-
+typedef enum logic [4:0] {
+    LOAD        = 5'h00,
+    INPUT       = 5'h02,
+    FETCH       = 5'h03,
+    AND         = 5'h05,
+    OR          = 5'h06,
+    XOR         = 5'h07,
+    TEST        = 5'h09,
+    COMPARE     = 5'h0a,
+    ADD         = 5'h0c,
+    ADDCY       = 5'h0d,
+    SUB         = 5'h0e,
+    SUBCY       = 5'h0f,
+    RS          = 5'h10,
+    RETURN      = 5'h15,
+    OUTPUT      = 5'h16,
+    STORE       = 5'h17,
+    CALL        = 5'h18,
+    JUMP        = 5'h1a,
+    RETURNI     = 5'h1c,
+    INTERRUPT   = 5'h1e
+} opcode_t;
 
 class generator;
     
@@ -33,7 +54,7 @@ import kcpsmx3_inc::*;
 
 class opcode_generator;
 
-    rand opcode_t opcode;
+    rand opcode_t op;
 
 endclass
 
@@ -41,6 +62,7 @@ module top();
 	
 	bit [5:0]  opcode;
     bit [3:0]  sx, sy, constant;
+	opcode_t op;
 	
 	initial begin 
 	generator g = new();
@@ -48,7 +70,7 @@ module top();
 	repeat (1024) begin
 	assert(g.randomize());
 	assert(og.randomize());
-	display("%b",opcode);
+	$display("%b",op);
 	g.write_mem();
 	end
 	end
