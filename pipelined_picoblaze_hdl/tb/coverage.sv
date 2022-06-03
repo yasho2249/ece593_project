baa39d6360fd694f05edb00556de96d20b5ed51b
Supreet Gulavani <sgsupreetgulavani@gmail.com>
2022-06-03 16:36:01 -0700

    output cov added

--- pipelined_picoblaze_hdl/tb/coverage.sv
+++ pipelined_picoblaze_hdl/tb/coverage.sv
@@ -43,12 +43,38 @@ module coverage_ifid (rojo_bfm bfm);
         rojob_reset :       coverpoint cp_Reset;
 
         // Crosspoints for inputs
-        rojob_in_portxrojob_interrupt : coverpoint rojob_in_port, rojob_interrupt;
-        rojob_in_portxrojob_reset :     coverpoint rojob_in_port, rojob_reset;
-        rojob_interruptxrojob_reset :   coverpoint rojob_interrupt, rojob_reset; 
+        rojob_in_portxrojob_interrupt : cross rojob_in_port, rojob_interrupt;
+        rojob_in_portxrojob_reset :     cross rojob_in_port, rojob_reset;
+        rojob_interruptxrojob_reset :   cross rojob_interrupt, rojob_reset; 
 
     endgroup
 
+    // Covergroup for Output Signals
+    covergroup cg_output_signals
+        rojob_out_portid: coverpoint port_id{
+            option.at_least = 1;
+            bins portid0 = {8'h00};
+            bins portid1 = {[8'h01:8'hFE]};
+            bins portid2 = {8'hFF};
+        }
+
+        rojob_out_writestrobe: coverpoint write_strobe;
+        rojob_out_readstrobe: coverpoint read_strobe;
+        rojob_out_interruptack: coverpoint interrupt_ack;
+        rojob_out_outport: coverpoint out_port{
+            option.at_least = 1;
+            bins outport0 = {8'h00};
+            bins outport1 = {[8'h01:8'hFE]};
+            bins outport2 = {8'hFF};
+        }
+
+        rojob_out_portidxreadstrobe: cross rojob_out_portid, rojob_out_readstrobe;
+        rojob_out_portidxwritestrobe: cross rojob_out_portid, rojob_out_writestrobe;
+        rojob_out_portidxinterruptack: cross rojob_out_portid, rojob_out_interruptack;
+        rojob_out_readstrobexinterruptack: cross rojob_out_readstrobe, rojob_out_interruptack;
+        rojob_out_writestrobexinterruptack: cross rojob_out_writestrobe, rojob_out_interruptack;
+    
+    endgroup
     /*
     // Covergroup for Instructions
     // Will this work?? 
@@ -58,4 +84,30 @@ module coverage_ifid (rojo_bfm bfm);
     endgroup
     */
 
-endmodule
+    cg_input_signals input_signals;
+    cg_output_signals output_signals;
+
+    initial begin: coverage_block
+  
+        input_signals = new();
+        output_signals = new();
+  
+        forever begin: sampling_block
+            @(posedge bfm.clk);
+            in_port = bfm.in_port;
+            interrupt = bfm.interrupt;
+            reset = bfm.reset;
+            port_id = bfm.port_id;
+            write_strobe = bfm.write_strobe;
+            read_strobe = bfm.read_strobe;
+            out_port = bfm.out_port;
+            interrupt_ack = bfm.interrupt_ack;
+
+	        input_signals.sample();
+	        output_signals.sample();
+	
+        end: sampling_block
+     end: coverage_block
+
+
+endmodule : coverage_ifid
