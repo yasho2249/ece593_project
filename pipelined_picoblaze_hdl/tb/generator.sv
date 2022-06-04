741dec42b6dda5075331664ef4efdddd6d354a7b
Supreet Gulavani <sgsupreetgulavani@gmail.com>
2022-06-04 01:54:44 -0700

    generator module

---
+++ pipelined_picoblaze_hdl/tb/generator.sv
@@ -0,0 +1,34 @@
+
+
+import transaction::*;
+
+class generator;
+    
+    // transaction class declaration
+    rand transaction txn;
+
+    // mailbox 
+    mailbox gen_driv;
+
+    // number of items to generate
+    int rep_count;
+
+    // end of transaction generation
+    event ended;
+
+    // constructor
+    function new(mailbox gen_driv);
+        this.gen_driv = gen_driv;
+    endfunction
+
+    //
+    task main();
+        repeat begin
+            txn = new();
+            if(!txn.randomize()) $fatal("txn randomization failed");
+            gen_driv.put(txn);
+        end
+        -> ended;
+    endtask
+
+endclass
