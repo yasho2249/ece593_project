448de8d65dd47898f357d57b2cfbb7aa2ba69d21
Supreet Gulavani <sgsupreetgulavani@gmail.com>
2022-06-04 18:59:54 -0700

    added more coverage

--- pipelined_picoblaze_hdl/tb/coverage.sv
+++ pipelined_picoblaze_hdl/tb/coverage.sv
@@ -7,24 +7,40 @@ ECE 593: Final Project
 Coverage module consist of Input covergroups 
 
 */
+`include "blockram.sv"
+`include "kcpsmx_register.sv"
+`include "kcpsmx_scratch.sv"
 
 
 module coverage_ifid (rojo_bfm bfm);
     
-    import rojobpkg::*;
+   // import rojobpkg::*;
 
-    // Inputs
+    // Inputs _rojo
     bit [PORT_WIDTH-1:0]    in_port;        // Port input
     bit                     interrupt;      // Interrupt req
     bit                     reset;          // Reset
     bit                     clk;             // Clock
 
-    // Outputs
+    // Outputs _rojo
     bit [PORT_DEPTH-1:0]   port_id;        // Port address.
     bit                    write_strobe;   // Port output strobe
     bit [PORT_WIDTH-1:0]   out_port;       // Port output
     bit                    read_strobe;    // Port input strobe
     bit                    interrupt_ack;  // Interrupt acknowledge
+
+
+    // blockram signals
+    bit en, we;
+    bit [DEPTH-1:0] ad;
+    bit [WIDTH-1:0] din;
+    bit [WIDTH-1:0] dout;
+
+    // scratch signals
+    bit write_enable;
+    bit [SCRATCH_DEPTH-1:0] address;
+    bit [SCRATCH_WIDTH-1:0] data_in;
+    bit [SCRATCH_WIDTH-1:0] data_out;
 
     // Covergroup for inputs
     covergroup cg_input_signals
@@ -43,11 +59,31 @@ module coverage_ifid (rojo_bfm bfm);
         rojob_reset :       coverpoint cp_Reset;
 
         // Crosspoints for inputs
+
+        //rojo
         rojob_in_portxrojob_interrupt : cross rojob_in_port, rojob_interrupt;
         rojob_in_portxrojob_reset :     cross rojob_in_port, rojob_reset;
         rojob_interruptxrojob_reset :   cross rojob_interrupt, rojob_reset; 
 
-    endgroup
+        // blockram
+        rojob_in_en : coverpoint en;
+        rojob_in_we : coverpoint we;
+        rojob_in_enxwe : cross rojob_in_en, rojob_in_we;
+        rojob_in_ad : coverpoint ad{
+            options.at_least = 1;
+            bins ad0 = {[DEPTH-1]'h00};
+            bins ad1 = {[DEPTH-1]'h01:[DEPTH-1]'hFF};
+        }
+        rojob_in_din : coverpoint din{
+            options.at_least = 1;
+            bins din0 = {[WIDTH-1]'h00};
+            bins din1 = {[WIDTH-1]'h01:[WIDTH-1]'hFF};
+        }
+
+        // scratch
+        rojob_in_write_enable : coverpoint write_enable;
+
+    endgroup 
 
     // Covergroup for Output Signals
     covergroup cg_output_signals
@@ -73,7 +109,15 @@ module coverage_ifid (rojo_bfm bfm);
         rojob_out_portidxinterruptack: cross rojob_out_portid, rojob_out_interruptack;
         rojob_out_readstrobexinterruptack: cross rojob_out_readstrobe, rojob_out_interruptack;
         rojob_out_writestrobexinterruptack: cross rojob_out_writestrobe, rojob_out_interruptack;
+
+        rojob_out_dout : coverpoint dout{
+            options.at_least = 1;
+            bins dout0 = {[WIDTH-1]'h00};
+            bins dout1 = {[WIDTH-1]'h01:[WIDTH-1]'hFF};
+        }
     
+        
+
     endgroup
     /*
     // Covergroup for Instructions
